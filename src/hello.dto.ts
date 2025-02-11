13ed4bf1f7e3ab114fd1da06a1b9b0b46657f67a
YehorKardashRadency <yehor.k@radency.com>
2025-02-11 13:09:48 +0200

    change dto

--- src/hello.dto.ts
+++ src/hello.dto.ts
@@ -1,7 +1,6 @@
-import { IsOptional, IsString } from 'class-validator';
+import { IsString } from 'class-validator';
 
 export class HelloDto {
   @IsString()
-  @IsOptional()
-  name?: string;
+  name: string;
 }
